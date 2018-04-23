b985b8e864f1ed7b7730c64b5bd8821d5fb699db
JordanRex <37971990+JordanRex@users.noreply.github.com>
2018-04-23 19:26:25 +0530

    Update PumpItUp.R

--- PumpItUp.R
+++ PumpItUp.R
@@ -97,6 +97,8 @@
 
 # Sample and Base Model
 {
+  gc()
+
   train = train_test_encoded %>%
     filter(tt == "train")
   test = train_test_encoded %>%
@@ -104,18 +106,20 @@
 
   # encode the dep variable as well
   dep_encoded = CatEncoders::LabelEncoder.fit(train_dep$status_group)
-  train_dep$status_group = CatEncoders::transform(dep_encoded, train_dep$status_group)
+  train_dep$status_group = CatEncoders::transform(dep_encoded, train_dep$status_group) - 1
 
-  train_sample = sample_frac(tbl = train, size = 0.75) %>%
-    mutate(tt = NULL)
+  train_sample = sample_frac(tbl = train, size = 0.7) %>%
+    select(-tt, -id)
   train_sample_dep_actual = train_sample %>%
     left_join(., train_dep) %>%
     select(id, status_group)
   test_sample = anti_join(train, train_sample, by = "id") %>%
-    mutate(tt = NULL)
+    select(-tt, -id)
   test_sample_dep_actual = test_sample %>%
     left_join(., train_dep) %>%
     select(id, status_group)
+  test_ids = test_sample_dep_actual %>%
+    .$id
 
   train_data_matrix = xgb.DMatrix(data = as.matrix(train_sample),
                                   label = as.matrix(train_sample_dep_actual$status_group))
@@ -124,17 +128,90 @@
 
   numberOfClasses = length(unique(train_dep$status_group))
 
-  xgb_params <- list("objective" = "multi:softprob",
-                     "eval_metric" = "mlogloss",
-                     "num_class" = numberOfClasses + 1)
-  nround = 100 # number of XGBoost rounds
+  # n-fold cv with random search on parameters
+  {
+    best_param = list()
+    best_seednumber = 1234
+    best_logloss = Inf
+    best_logloss_index = 0
+
+    for (iter in 1:25) {
+      param = list(objective = "multi:softprob",
+                   eval_metric = "mlogloss",
+                   num_class = numberOfClasses,
+                   max_depth = sample(10:15, 1),
+                   eta = runif(1, .01, .2),
+                   gamma = runif(1, 0.0, 0.2),
+                   subsample = runif(1, .7, .9),
+                   colsample_bytree = runif(1, .5, .7),
+                   min_child_weight = sample(1:10, 1),
+                   max_delta_step = sample(1:10, 1)
+      )
+      cv.nround = sample(seq.int(100, 500, 100), 1)
+      cv.nfold = 5
+      seed.number = sample.int(10000, 1)[[1]]
+      set.seed(seed.number)
+      mdcv <- xgb.cv(data=train_data_matrix, params = param, stratified = T, print_every_n = 50,
+                     nfold = cv.nfold, nrounds = cv.nround,
+                     verbose = T, early_stopping_rounds = 10, maximize = FALSE)
+
+      min_logloss = min(mdcv$evaluation_log$test_mlogloss_mean)
+      min_logloss_index = which.min(mdcv$evaluation_log$test_mlogloss_mean)
+
+      if (min_logloss < best_logloss) {
+        best_logloss = min_logloss
+        best_logloss_index = min_logloss_index
+        best_seednumber = seed.number
+        best_param = param
+      }
+      print(paste0("iter is ", i))
+    }
+
+    nround = best_logloss_index
+    set.seed(best_seednumber)
+  }
+
+  best_params_df = data.frame(best_param) %>%
+    cbind(., data.frame(nround = nround))
+  saveRDS(best_params_df, 'best_params_df.rds')
+
+  train_full_matrix = xgb.DMatrix(data = as.matrix(train %>% select(-tt, -id)),
+                                  label = as.matrix(train_dep$status_group))
+  test_full_matrix = xgb.DMatrix(data = as.matrix(test %>% select(-tt, -id)))
+
+  xgb_params = best_param
+  nround = nround
   cv.nfold = 5
 
   # Fit cv.nfold * cv.nround XGB models and save OOF predictions
   cv_model = xgb.cv(params = xgb_params,
-                     data = train_data_matrix,
-                     nrounds = nround,
-                     nfold = cv.nfold,
-                     verbose = FALSE,
-                     prediction = TRUE)
+                    data = train_full_matrix,
+                    nrounds = nround,
+                    nfold = cv.nfold,
+                    verbose = T,
+                    prediction = T,
+                    print_every_n = 25,
+                    early_stopping_rounds = 10)
+
+  OOF_prediction = data.frame(cv_model$pred) %>%
+    mutate(max_prob = max.col(., ties.method = "last") - 1,
+           label = train_dep$status_group,
+           pred_flag = if_else(max_prob == label, 1, 0))
+
+  md = xgb.train(data = train_full_matrix,
+                 params = best_param,
+                 nrounds = nround,
+                 verbose = T,
+                 print_every_n = 50,
+                 early_stopping_rounds = 10)
+
+  test_ids = test$id
+  test_final_predictions = predict(md, test_full_matrix, reshape = T)
+  output = data.frame(test_final_predictions) %>%
+    mutate(status_group = max.col(., ties.method = "last")) %>%
+    cbind(data.frame(id = test_ids), .) %>%
+    select(id, status_group)
+  output$status_group = CatEncoders::inverse.transform(dep_encoded, output$status_group)
+
+  fwrite(output, 'output.csv')
 }
