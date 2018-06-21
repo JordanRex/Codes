48e2cb2646d7ed7788a0dee0142574d53ee61e83
Varun Rajan <varunrajan@protonmail.com>
2018-06-21 17:11:03 +0530

    Update Modelling snippets.R

--- Codes/Modelling snippets.R
+++ Codes/Modelling snippets.R
@@ -97,7 +97,7 @@ xgb_importance_fn_2 = function(xgbmodel, train, response = "dep", id = "id", n =
 
 #--------------------------------------------------------------------------------------------------------------------------------#
 
-### custom function modified from MLBayesOpt package for bayesian hyper-parameter tuning
+### custom function modified from MLBayesOpt package for bayesian hyper-parameter tuning ###
 # can perform tuning for classification (binary/multi) and regression
 # needs more testing for special cases and other parameters
 # provide ranges for parameters if required (defaults provided), needs what acquisition method to be used for the bayesian segment
@@ -165,7 +165,7 @@ xgb_opt_custom = function(train_df, test_df, test_label, objectfun,
   return(opt_res)
 }
 
-# example #
+# example ->
 {
 # xgb_bayes_output = xgb_opt_custom(train_df = train_xgb, test_df = test_xgb, test_label = test_label,
 #                                   objectfun = "reg:linear", evalmetric = "rmse",
@@ -185,6 +185,63 @@ xgb_opt_custom = function(train_df, test_df, test_label, objectfun,
 #
 # xgb_model = xgb.train(params = params, nrounds = xgb_bayes_output$Best_Par["nrounds_opt"], data = train_xgb, verbose = T, print_every_n = 10)
 }
+
+#--------------------------------------------------------------------------------------------------------------------------------#
+
+### function to do n-fold cv xgboost ###
+# used for parameter tuning with grid/random search without a train/test split but based off on n-fold cv results
+# define folds, hardcoded here for multi-class classification. subtle changes required to generalize
+# n-fold cv with random search on parameters
+nfold_xgb_fn = function(train_xgb, response, folds = 5, maximize = TRUE, evalmetric = "mlogloss", n = 20) {
+  best_param = list()
+  best_seednumber = 1234
+  best_score = Inf
+  best_score_index = 0
+
+  for (iter in 1:n) {
+    param = list(objective = "multi:softprob",
+                 eval_metric = evalmetric,
+                 num_class = length(unique(response[, 1])),
+                 max_depth = sample(5:15, 1),
+                 eta = runif(1, .01, .2),
+                 gamma = runif(1, 0.0, 0.2),
+                 subsample = runif(1, .7, .9),
+                 colsample_bytree = runif(1, .5, .7),
+                 min_child_weight = sample(1:10, 1),
+                 max_delta_step = sample(1:10, 1)
+    )
+    cv.nround = sample(seq.int(100, 500, 100), 1)
+    cv.nfold = folds
+    seed.number = sample.int(10000, 1)[[1]]
+    set.seed(seed.number)
+    mdcv <- xgb.cv(data=train_data_matrix, params = param, stratified = T, print_every_n = 50,
+                   nfold = cv.nfold, nrounds = cv.nround,
+                   verbose = T, early_stopping_rounds = 10, maximize = maximize)
+
+    evalmetric_col = paste0("test_", evalmetric, "_mean")
+
+    min_score = min(mdcv$evaluation_log[[evalmetric_col]])
+    min_score_index = which.min(mdcv$evaluation_log[[evalmetric_col]])
+
+    if (min_score < best_score) {
+      best_score = min_score
+      best_score_index = min_score_index
+      best_seednumber = seed.number
+      best_param = param
+    }
+    print(paste0("iter is ", i))
+  }
+
+  nround = best_score_index
+  set.seed(best_seednumber)
+
+  best_params_df = data.frame(best_param) %>%
+    cbind(., data.frame(nround = nround))
+  saveRDS(best_params_df, 'best_params_df.rds')
+
+  return(best_params_df)
+}
+
 ##################################################################################################################################
 
 ## RANGER(RF) ##
@@ -195,6 +252,7 @@ xgb_opt_custom = function(train_df, test_df, test_label, objectfun,
 
 ## HTS/FORECAST ##
 ##################################################################################################################################
+
 ### function to create an hts/gts object ###
 # make a singular id column, singular time dimension, and specify characters convention used for creating the id column
 # example -> id_col = store + department = S{1-9} + D{1-99} = S01D01 = characters(2,3)
@@ -210,6 +268,7 @@ train_hts_fn = function(train, id_col = "id", time_col = "date", dep_col = "dep"
   return(train_ts)
 }
 
+#--------------------------------------------------------------------------------------------------------------------------------#
 
 ### function to create and return an hts model
 # specify the train ts object, train/test xreg objects (only if you have regressors! else skip argument), fmethod to be used (ets|arima|hw?), seasonal = T or F, h = horizon to be predicted
@@ -217,8 +276,41 @@ hts_model_fn = function(train_ts, train_xreg = NULL, test_xreg = NULL, h = 12, f
   hts_model = forecast(object = train_ts, xreg = train_xreg, newxreg = test_xreg, h = h, fmethod = fmethod, seasonal = seasonal)
   return(hts_model)
 }
+# save to hts_model, predictions are saved in hts_model$bts. Bind with test_ts$bts to get output dataframe
+# output_hts = rbind(hts_model$bts, test_ts %>% .$bts)
 
+##################################################################################################################################
 
+## GLMNET for text features selection ##
+##################################################################################################################################
+
+### function to select the top n relevant features after an NLP process
+# set for classification (binary) -> modify accordingly for regression/multi-class classification
+# needs to be tested for whether it works in the form of a function, intended for leveraging, not as in usage
+glmnet_fn = function(train, train_dep, test_dep, response = "target") {
+  NFOLDS = 5
+  t1 = Sys.time()
+  glmnet_classifier = cv.glmnet(x = train, y = train_dep[[response]],
+                                family = 'binomial',
+                                # L1 penalty
+                                alpha = 1,
+                                # interested in the area under ROC curve
+                                type.measure = "auc",
+                                # 5-fold cross-validation
+                                nfolds = NFOLDS,
+                                # high value is less accurate, but has faster training
+                                thresh = 1e-5,
+                                # again lower number of iterations for faster training
+                                maxit = 1e7)
+  print(difftime(Sys.time(), t1, units = 'sec'))
+
+  plot(glmnet_classifier)
+  print(paste("max AUC =", round(max(glmnet_classifier$cvm), 4)))
+
+  preds = as.numeric(predict(glmnet_classifier, train, type = 'response'))
+
+  glmnet:::auc(test_dep[[response]], preds)
+}
 
 ##################################################################################################################################
 
@@ -228,6 +320,7 @@ hts_model_fn = function(train_ts, train_xreg = NULL, test_xreg = NULL, h = 12, f
 # the arguments are the train, test dataframes; and the character columns' subset and numeric dependant variable dataframes
 # works best for regression problems (forecasting)
 # for classification convert the dependant to a numeric (label encoded) column
+# mode and skew needs to be tested, also select whatever functions ONLY if you know what you are doing
 feateng_categtoDeviationenc_fn = function(train, test, dep_var) {
     train_char_data = train %>% select_if(function(col) is.character(col) | is.logical(col)) %>%
       mutate_all(as.character)
@@ -235,8 +328,9 @@ feateng_categtoDeviationenc_fn = function(train, test, dep_var) {
     for (i in 1:ncol(train_char_data)) {
       temp_col = colnames(train_char_data[, i, drop = F])
 
-      dev_encod_cols = paste0(temp_col, c("_mean", "_min", "_max", "_sd", "_median", "_ndistinct"))
-      dev_funs = paste0(c("mean(", "min(", "max(", "sd(", "median(", "n_distinct("), "dep", ")") %>% as.list
+      dev_encod_cols = paste0(temp_col, c("_mean", "_min", "_max", "_sd", "_median", "_ndistinct", "_mode", "_skew"))
+      dev_funs = paste0(c("mean(", "min(", "max(", "sd(", "median(", "n_distinct("), "dep", ")") %>%
+        append(., c("mlv(dep, method = 'mfv')[['M']]", "mlv(dep, method = 'mfv')[['skewness']]")) %>% as.list
 
       temp = train_char_data[, i, drop = F] %>%
         bind_cols(., dep_var %>% set_colnames("dep")) %>%
@@ -256,10 +350,8 @@ feateng_categtoDeviationenc_fn = function(train, test, dep_var) {
 #--------------------------------------------------------------------------------------------------------------------------------#
 
 ### function to do bucket feature creation ###
-# supervised, uses dependant variable mean and sd with each level in the categorical features
-# preferred for a categorical variable if the number of levels it contains is < 50-100 (argument n gives this control)
-#
-feateng_bucket_fn = function(train, num_levels = 50) {
+# unsupervised, create simply by specifying train set
+feateng_bucket_fn_unsup = function(train) {
   temp = train
 
   temp_categ = temp %>%
@@ -279,4 +371,71 @@ feateng_bucket_fn = function(train, num_levels = 50) {
       summarise()
   }
 }
+
+# supervised, uses dependant variable mean and sd with each level in the categorical features
+# preferred for a categorical variable if the number of levels it contains is < 50-100 (argument n gives this control)
+feateng_bucket_fn_sup = function(train, dep, col) {
+  temp = train %>% select_(indep = col, dep)
+  temp_test = test %>% select_(indep = col)
+  temp_extreme_cuts = append(temp$indep, temp_test$indep) %>% unique %>% range(.) #%>% .[. %in% c(max(.), min(.))]
+
+  temp_model = rpart::rpart(data = temp, formula = paste0(dep, "~ indep"))
+  temp_splits = temp_model$splits[ ,4] %>% as.numeric %>% append(., temp_extreme_cuts) %>% sort %>% unique
+
+  if (is.null(temp_model$splits)) { temp_splits %<>% append(., mean(temp$indep)) %>% sort %>% unique }
+
+  temp_train_cut = data.frame(temp = cut(temp$indep, breaks = temp_splits, labels = F, include.lowest = T)) %>%
+    set_colnames(value = paste0("bucket_", col)) %>% bind_cols(., temp %>% select(indep) %>% set_colnames(col)) %>%
+    distinct
+  temp_test_cut = data.frame(temp = cut(temp_test$indep, breaks = temp_splits, labels = F, include.lowest = T)) %>%
+    set_colnames(value = paste0("bucket_", col)) %>% bind_cols(., temp_test %>% select(indep) %>% set_colnames(col)) %>%
+    distinct
+  train %<>% left_join(., temp_train_cut)
+  test %<>% left_join(., temp_test_cut)
+
+  train <<- train
+  test <<- test
+}
+
+# example usage ->
+# bucket_feats = train %>%
+#   summarise_all(n_distinct) %>%
+#   melt %>%
+#   set_colnames(c("column", "count")) %>%
+#   mutate(bucket_feat_flag = if_else(count >= threshold_ndistinct, 1, 0)) %>%
+#   filter(bucket_feat_flag == 1) %>% .$column %>% as.character
+#
+# for (i in 1:length(bucket_feats)) {
+#   feateng_bucket_fn(train, bucket_feats[i])
+#}
+
+#--------------------------------------------------------------------------------------------------------------------------------#
+
+### function to d0 label encoding ###
+# label encode and store all character columns for algorithms that require a numeric matrix
+labelencoder_fn = function(train, test, response) {
+  categ_cols = train %>% select_if(is.character) %>% colnames
+  rem_cols = setdiff(colnames(train), categ_cols)
+
+  train_categ = train %>% select(categ_cols)
+  test_categ = test %>% select(categ_cols)
+  train_test_categ = bind_rows(train_categ, test_categ)
+
+  train_rest = train %>% select(rem_cols) %>% mutate(tt = "train")
+  test_rest = test %>% select(rem_cols) %>% mutate(tt = "test")
+  train_test_rest = bind_rows(train_rest, test_rest)
+
+  encoded_list = list()
+
+  for (i in 1:length(categ_cols)) {
+    encoded_list[[i]] = CatEncoders::LabelEncoder.fit(train_test_categ[, i])
+    train_test_categ[, i] = CatEncoders::transform(encoded_list[[i]], train_test_categ[, i])
+  }
+
+  train_test_encoded = train_test_categ %>%
+    bind_cols(., train_test_rest)
+
+  return(train_test_encoded)
+}
+
 ##################################################################################################################################
