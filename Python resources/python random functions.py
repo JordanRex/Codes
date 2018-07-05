e8c00796b9c08c661ef1cfb9cbcb21292dfc99fc
Varun Rajan <varunrajan@protonmail.com>
2018-07-05 18:23:18 +0530

    random functions base text file pushed

--- Python resources/python random functions.py
+++ Python resources/python random functions.py
@@ -1,16 +1,13 @@
-import os
+## python function for various purposes
 
-def Is64Windows():
-    return 'PROGRAMFILES(X86)' in os.environ
+# 1. creating a function definition
+def function_name (..., arguments here):
+	# function/operation to be given here
+	# either simply execute the operation or assign it to a variable
+	return # you can return the variable, a print statement, or if blank;
+	# will return value of operation above assuming it is not assigned to any variable;
+	# if assigned to variable in local scope, and return is empty; function returns nothing
 
-def GetProgramFiles32():
-    if Is64Windows():
-        return os.environ['PROGRAMFILES(X86)']
-    else:
-        return os.environ['PROGRAMFILES']
-
-def GetProgramFiles64():
-    if Is64Windows():
-        return os.environ['PROGRAMW6432']
-    else:
-        return None
+# 2. some regex tips
+	- use prefix 'r' if the pattern contains escape sequences
+	- 
